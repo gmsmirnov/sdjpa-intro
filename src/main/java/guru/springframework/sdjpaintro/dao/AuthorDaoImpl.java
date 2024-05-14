b75e989bfc707ced001993c104538cd1cee2b0f2
gsmirnov <gsmirnov@callinsight.ru>
2024-05-14 21:55:27 +0700

    version 0.0.26
    Create Author DAO.

---
+++ src/main/java/guru/springframework/sdjpaintro/dao/AuthorDaoImpl.java
@@ -0,0 +1,12 @@
+package guru.springframework.sdjpaintro.dao;
+
+import guru.springframework.sdjpaintro.domain.Author;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthorDaoImpl implements AuthorDao {
+    @Override
+    public Author getById(Long id) {
+        return null;
+    }
+}
---
+++ src/test/java/guru/springframework/sdjpaintro/AuthorDaoIntegrationTest.java
@@ -0,0 +1,32 @@
+package guru.springframework.sdjpaintro;
+
+import guru.springframework.sdjpaintro.dao.AuthorDao;
+import guru.springframework.sdjpaintro.dao.AuthorDaoImpl;
+import guru.springframework.sdjpaintro.domain.Author;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ActiveProfiles("local")
+@DataJpaTest
+@ComponentScan(basePackages = {"guru.springframework.sdjpaintro.dao"})
+//@Import(AuthorDaoImpl.class) // если @ComponentScan не работает в тесте (не работал в старой версии SpringBoot, теперь исправили)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class AuthorDaoIntegrationTest {
+
+    @Autowired
+    AuthorDao authorDao;
+
+    @Test
+    void testGetAuthor() {
+        Author author = authorDao.getById(1L);
+
+        assertThat(author).isNotNull();
+    }
+}
