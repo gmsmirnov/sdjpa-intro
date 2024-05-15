1d0bae473ae0eba0b5b1e2382af8c9f5f5d8ed48
gsmirnov <gsmirnov@callinsight.ru>
2024-05-15 14:43:11 +0700

    version 0.0.27
    Implement get author by id.
    Release DB resources.

--- src/main/java/guru/springframework/sdjpaintro/dao/AuthorDaoImpl.java
+++ src/main/java/guru/springframework/sdjpaintro/dao/AuthorDaoImpl.java
@@ -1,12 +1,38 @@
 package guru.springframework.sdjpaintro.dao;
 
 import guru.springframework.sdjpaintro.domain.Author;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.*;
 
 @Component
 public class AuthorDaoImpl implements AuthorDao {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public AuthorDaoImpl(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Override
     public Author getById(Long id) {
+        try (Connection connection = dataSource.getConnection();
+             Statement statement = connection.createStatement()) {
+            ResultSet rs = statement.executeQuery("SELECT * FROM author WHERE id = " + id);
+            if (rs.next()) {
+                Author author = new Author();
+                author.setId(id);
+                author.setFirstName(rs.getString("first_name"));
+                author.setLastName(rs.getString("last_name"));
+                return author;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
         return null;
     }
 }
--- src/test/java/guru/springframework/sdjpaintro/AuthorDaoIntegrationTest.java
+++ src/test/java/guru/springframework/sdjpaintro/AuthorDaoIntegrationTest.java
@@ -1,14 +1,12 @@
 package guru.springframework.sdjpaintro;
 
 import guru.springframework.sdjpaintro.dao.AuthorDao;
-import guru.springframework.sdjpaintro.dao.AuthorDaoImpl;
 import guru.springframework.sdjpaintro.domain.Author;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
