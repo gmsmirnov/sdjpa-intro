52f32e8a1b152f8e3ae5f9cdd467c64c9493e1b0
gsmirnov <gsmirnov@callinsight.ru>
2024-05-16 23:21:26 +0700

    version 0.0.34
    Get author by id with JDBC Template.

--- src/main/java/guru/springframework/sdjpaintro/dao/AuthorDaoImpl.java
+++ src/main/java/guru/springframework/sdjpaintro/dao/AuthorDaoImpl.java
@@ -2,93 +2,45 @@ package guru.springframework.sdjpaintro.dao;
 
 import guru.springframework.sdjpaintro.domain.Author;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
-import javax.sql.DataSource;
-import java.sql.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 @Component
 public class AuthorDaoImpl implements AuthorDao {
 
-    private final DataSource dataSource;
+    private final JdbcTemplate jdbcTemplate;
 
     @Autowired
-    public AuthorDaoImpl(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public AuthorDaoImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     @Override
     public Author getById(Long id) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement ps = connection.prepareStatement("SELECT * FROM author WHERE id = ?")) {
-            ps.setLong(1, id);
-            ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
-                return getAuthorFromResultSet(rs);
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-        return null;
+        return jdbcTemplate.queryForObject("SELECT * FROM author WHERE id = ?", getRowMapper(), id);
     }
 
     @Override
     public Author findAuthorByName(String firstName, String lastName) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement ps = connection.prepareStatement("SELECT * FROM author a WHERE a.first_name = ? AND a.last_name = ?")) {
-            ps.setString(1, firstName);
-            ps.setString(2, lastName);
-            ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
-                return getAuthorFromResultSet(rs);
-            }
-            return null;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        return null;
     }
 
     @Override
     public Author saveNewAuthor(Author author) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement ps = connection.prepareStatement("INSERT INTO author (first_name, last_name) VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS)) {
-            ps.setString(1, author.getFirstName());
-            ps.setString(2, author.getLastName());
-            ps.executeUpdate();
-            ResultSet generatedKeys = ps.getGeneratedKeys();
-            if (generatedKeys.next()) {
-                author.setId(generatedKeys.getLong(1));
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return author;
+        return null;
     }
 
     @Override
     public void updateAuthor(Author author) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement ps = connection.prepareStatement("UPDATE author SET first_name = ?, last_name = ? WHERE id = ?")) {
-            ps.setString(1, author.getFirstName());
-            ps.setString(2, author.getLastName());
-            ps.setLong(3, author.getId());
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     @Override
     public void deleteAuthor(Author author) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement ps = connection.prepareStatement("DELETE FROM author WHERE id = ?")) {
-            ps.setLong(1, author.getId());
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     private Author getAuthorFromResultSet(ResultSet rs) throws SQLException {
